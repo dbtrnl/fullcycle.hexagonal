1ed2ef65020fa8590f23457818b312e3a508200e
dbtrnl <daniel.bittar@alumni.usp.br>
2021-06-06 01:47:33 -0300

    Create DB Adapter/Save method with test

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -18,14 +18,53 @@ func NewProductDb(db *sql.DB) *ProductDb {
 func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 	var product application.Product
 
+	// Working with prepared statements to avoid SQL Injection
 	statement, err := p.db.Prepare("SELECT id, name, price, status FROM products WHERE id=?")
-	if err != nil {
-		return nil, err
-	}
+	if err != nil {	return nil, err	}
 
 	err = statement.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
-	if err != nil {
-		return nil, err
-	}
+	if err != nil {	return nil, err	}
+
 	return &product, nil
+}
+
+func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
+	var rows int
+	p.db.QueryRow("select id from products where id=?", product.GetId()).Scan(&rows)
+
+	if rows == 0 {
+		_, err := p.create(product)
+		if err != nil { return nil, err }
+	} else {
+		_, err := p.update(product)
+		if err != nil { return nil, err }
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
+	statement, err := p.db.Prepare(`INSERT INTO products(id, name, price, status) values(?,?,?,?);`)
+	if err != nil {	return nil, err	}
+
+	_, err = statement.Exec(
+		product.GetId(),
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+	)
+	if err != nil { return nil, err }
+
+	err = statement.Close()
+	if err != nil { return nil, err }
+
+	return product, nil
+}
+
+func (p *ProductDb) update(product application.ProductInterface) (application.ProductInterface, error) {
+	_, err := p.db.Exec("update products set name=?, price=?, status=? where id=?",
+		product.GetName(), product.GetPrice(), product.GetStatus(), product.GetId())
+	if err != nil {	return nil, err	}
+
+	return product, nil
 }
--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/dnbtr/fullcycle.hexagonal/adapters/db"
+	"github.com/dnbtr/fullcycle.hexagonal/application"
 	"github.com/stretchr/testify/require"
 )
 
@@ -26,18 +27,17 @@ func createTable(db *sql.DB) {
 			);`
 
 	statement, err := db.Prepare(tableQuery)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	if err != nil {	log.Fatal(err.Error()) }
+
 	statement.Exec()
 }
 
 func createProduct(db *sql.DB) {
 	insertQuery := `INSERT INTO products VALUES("idTest","Test product",0,"disabled");`
+
 	statement, err := db.Prepare(insertQuery)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	if err != nil {	log.Fatal(err.Error()) }
+
 	statement.Exec()
 }
 
@@ -53,4 +53,30 @@ func TestProductDb_Get(t *testing.T) {
 	require.Equal(t, "Test product", product.GetName())
 	require.Equal(t, 0.0, product.GetPrice())
 	require.Equal(t, "disabled", product.GetStatus())
+}
+
+func TestProductDb_Save(t *testing.T) {
+	setUp()
+	defer TestDatabase.Close()
+
+	productDb := db.NewProductDb(TestDatabase)
+
+	product := application.NewProduct()
+	product.Name = "Test product"
+	product.Price = 25
+
+	// Testing create()
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	// Testing update()
+	product.Status = "enabled"
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
 }
